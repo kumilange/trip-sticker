e027aaacc59c311dcd0bd7620cc812f25be132d8
haraKumi <email.935@gmail.com>
2017-08-12 14:50:07 +0900

    add toast

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import Map from '../containers/Map'
 import Modal from '../containers/Modal'
+import Toast from './Toast'
 import './App.css';
+import logo from '../img/logo.png'
 
 const App = (props) => {
   return (
     <div className="App">
       <header>
-        <h1><a href="/">Trip Sticker</a></h1>
+        <h1><a href="/" className="logo-link"><img src={logo} alt="logo" className="logo"/>Trip Sticker</a></h1>
       </header>
       <main className="main">
          <div id="map">
@@ -16,6 +18,7 @@ const App = (props) => {
         </div>
       </main>
       <Modal />
+      <Toast />
     </div>
   );
 }
