37838f806b91a23d376e0e8943c77971f4f0244b
haraKumi <email.935@gmail.com>
2017-09-17 20:31:41 +0900

    update www

--- knex.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  client: 'pg',
-  connection: process.env.DATABASE_URL || `postgres://${process.env.USER}@127.0.0.1:5432/tripsticker`,
-  migrations: {
-    tableName: 'knex_migrations'
-  }
-};
