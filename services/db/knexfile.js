b77897ce8a483b8d5cd57fa752573bcc229d12c0
haraKumi <email.935@gmail.com>
2017-09-17 19:07:33 +0900

    change the command to start for heroku

--- services/db/knexfile.js
+++ services/db/knexfile.js
@@ -1,15 +1,9 @@
-module.exports = {
+const knex = require('knex');
+
+const db = knex({
   client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    database: 'tripsticker'
-  },
-  port: 5432,
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
-};
+  connection: process.env.DATABASE_URL || `postgres://${process.env.USER}@127.0.0.1:5432/tripsticker`,
+  searchPath: 'public',
+});
+
+module.exports = db;
