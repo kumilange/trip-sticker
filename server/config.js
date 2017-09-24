e7c342735a7f825d845cbf906d4020a4cbbb7329
haraKumi <email.935@gmail.com>
2017-09-24 21:51:31 +0900

    Set DATABASE_URL for production

--- server/config.js
+++ server/config.js
@@ -2,10 +2,7 @@ module.exports = {
   // database connection configs
   db: {
     client: 'pg',
-    connection: {
-      host: '127.0.0.1',
-      database: 'tripsticker'
-    },
+    connection: process.env.DATABASE_URL || `postgres://${process.env.USER}@127.0.0.1:5432/tripsticker`,
     port: 5432
   },
 
