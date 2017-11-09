8f400540436e19145a0ed5956fe8bb396b184ffe
Kumiko Haraguchi <kumiko@codechrysalis.io>
2017-11-09 19:54:21 +0900

    Revert change to run on local

--- server/config.js
+++ server/config.js
@@ -2,7 +2,10 @@ module.exports = {
   // database connection configs
   db: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || `postgres://${process.env.USER}@127.0.0.1:5432/tripsticker`,
+    connection: {
+      host: '127.0.0.1',
+      database: 'tripsticker'
+    },
     port: 5432
   },
 };
